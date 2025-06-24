6c5ecd06d851ec9341109592df648c64e167327d
jexidev <169895029+jexidev@users.noreply.github.com>
2025-06-24 20:10:54 +0100

    Added function for get_choice from player

--- engine/scene_engine.py
+++ engine/scene_engine.py
@@ -1,5 +1,21 @@
+# Used inside scenes to hihglight narration
 def narrate(text):
     print(f"\n{text}")
 
+# Used inside scenes to separate speach lines.
 def speak(name, text):
     print(f'\n{name}: "{text}"')
+
+# Presents a list of options to the player  
+def get_choice(options):
+    print("\nWhat will you do?\n")
+    for i, option in enumerate(options, start=1):
+        print(f"{i}. {option}")
+
+    while True:
+        choice = input("\nChoose an option: ")
+        if choice.isdigit():
+            index = int(choice) - 1
+            if 0 <= index < len(options):
+                return index
+        print("Invalid choice. Please enter the number of your selection.")
