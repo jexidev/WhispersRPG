b24b2beb0b45348e731f4b95ccce5f5652551df6
jexidev <169895029+jexidev@users.noreply.github.com>
2025-06-27 11:22:25 +0100

    Added update_stat function to player.

--- player.py
+++ player.py
@@ -1,5 +1,24 @@
 player = {
     "name": "",
-    "motivation": None,
-    "alignment": 0
+    "stats": {
+        "identity_stability":0,
+        "insight":0,
+        "willpower":0,
+        "empathy":0,
+        "curiosity":0,
+        "corruption":0    
+    },
+    "flags": {
+        # Placeholder for adding flags later
+        # e.g. "refused_name_entry": False
+    },
 }
+
+def update_stat(player, stat_name, amount):
+    stats = player['stats']
+
+    if stat_name in stats:
+        stats[stat_name] += amount
+    
+    if stats[stat_name] < 0:
+        stats[stat_name] = 0
