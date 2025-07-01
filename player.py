3b936924d335a7e1650cb7e452b16e8bfb2b8289
jexidev <169895029+jexidev@users.noreply.github.com>
2025-07-01 11:35:57 +0100

    Converted player dictionaries into a class, to make OOP easier later.

--- player.py
+++ player.py
@@ -1,31 +1,34 @@
 from stat_feedback import increase_stat_feedback, decrease_stat_feedback
+from engine.scene_engine import narrate
 
-player = {
-    "name": "",
-    "stats": {
-        "identity_stability":2,
-        "insight":0,
-        "willpower":0,
-        "empathy":0,
-        "curiosity":0,
-        "corruption":0    
-    },
-    "flags": {
-        "discarded_ticket": False,
-    },
-}
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.stats = {        
+            "identity_stability":2,
+            "insight":0,
+            "willpower":0,
+            "empathy":0,
+            "curiosity":0,
+            "corruption":0 
+            }
+        self.flags = {
+            "discarded_ticket": False,}
+        self.inventory = []
+        self.journal = []
 
-def update_stat(player, stat_name, amount):
-    stats = player['stats']
+    def update_stat(self, stat_name, amount):
+        if stat_name in self.stats:
+            self.stats[stat_name] += amount
+            if self.stats[stat_name] < 0:
+                self.stats[stat_name] = 0
 
-    if stat_name in stats:
-        stats[stat_name] += amount
-        if stats[stat_name] < 0:
-            stats[stat_name] = 0
-        
-        if amount > 0:
-            message = increase_stat_feedback.get(stat_name)
-        elif amount < 0:
-            message = decrease_stat_feedback.get(stat_name)
-             
-    return message
+            if amount > 0:
+                feedback = increase_stat_feedback.get(stat_name)
+            elif amount < 0:
+                feedback = decrease_stat_feedback.get(stat_name)
+            else:
+                feedback = None
+
+            if feedback:
+                narrate(feedback)
